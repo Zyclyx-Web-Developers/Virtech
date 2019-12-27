513a944655239391e90802afd364427e539dfcea
srikanthkanjarla <srikanthkanjarla@gmail.com>
2019-12-27 15:48:02 +0530

    Fix chatbot initial no response issue

--- js/chatBot.js
+++ js/chatBot.js
@@ -16,12 +16,19 @@ function sessionTimeOut(){
 function getNewSession(){
   fetch('https://pure-tor-36404.herokuapp.com/session') 
   .then(function(session){
+    document.getElementById('userText').setAttribute("disabled", true);
+    document.querySelector('.sendBtn').setAttribute("disabled", true);
+    document.querySelector('.sendBtn').innerHTML = `<span class="spinner-border spinner-border-sm text-white" role="status">
+    <span class="sr-only">Loading...</span></span>`;
     return session.json();
   })
   .then(function(data){     
     sessionID = data.session;     
   })
   .then(function(){
+    document.getElementById('userText').removeAttribute("disabled");
+    document.querySelector('.sendBtn').removeAttribute("disabled");
+    document.querySelector('.sendBtn').innerHTML = ` <i class="fas fa-paper-plane text-white"></i>`;
     if(timeOutID){
       clearTimeout(timeOutID);
     }
@@ -51,7 +58,7 @@ document
   let userText = document.getElementById("userText").value;
 
   // if user input not empty send API Request to chat bot
-  if(userText !== ''){
+  if(userText !== '' && sessionID){
   messagesElement.innerHTML += `<p id="userReplay">${userText}</p>`;
   
   // disable user input
@@ -61,7 +68,6 @@ document
   <span class="sr-only">Loading...</span></span>`;
   document.getElementById("userText").value = "";
 
-if(sessionID){
   fetch("https://pure-tor-36404.herokuapp.com/virtech", {
     method: "post",
     headers: {
@@ -114,7 +120,6 @@ if(sessionID){
     .catch(function(error){
       console.log(error);
     })
-  }
   }
 });
 
